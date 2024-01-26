c4721616b00656a19ce8ecdf26184405189b93f6
Mike Magnus <mikemagnus@gmail.com>
2024-01-26 11:11:24 -0700

    Added MainMenu, started fleshing out initialization

---
+++ ClassicRogueLike/MainMenu.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <SDL.h>
+#include <SDL_keycode.h>
+
+//References to the SDL2 Objects for the window and Renderer
+SDL_Window* mainWindow;
+SDL_Renderer* mainRenderer;
+
+bool init();
+bool load();
+
+bool initialized;
+bool quit;
+
+int main(int argc, char** args)
+{
+	quit = false;
+
+	if (argc > 1)
+	{
+		//Handle Potential Arguments
+	}
+
+	if (!init())
+	{
+		std::cout << "Error Initializing" << SDL_GetError() << std::endl;
+	}
+
+	initialized = true;
+
+	if (!load())
+	{
+		std::cout << "Error Loading Assets" << SDL_GetError() << std::endl;
+	}
+
+}
