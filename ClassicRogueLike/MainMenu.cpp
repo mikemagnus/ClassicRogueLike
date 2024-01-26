af31d02b61d95773e1f7473491493d503b2129bd
Mike Magnus <mikemagnus@gmail.com>
2024-01-26 13:05:25 -0700

    Switching to VS2022

--- ClassicRogueLike/MainMenu.cpp
+++ ClassicRogueLike/MainMenu.cpp
@@ -34,4 +34,44 @@ int main(int argc, char** args)
 		std::cout << "Error Loading Assets" << SDL_GetError() << std::endl;
 	}
 
+	loaded = true;
+
+	while (!quit)
+	{
+
+	}
+}
+
+bool init()
+{
+	// Initialize SDL. SDL_Init will return -1 if it fails.
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) 
+	{
+		return false;
+	}
+
+	// Create our window
+	mainWindow = SDL_CreateWindow("ClassicRogueLike", 0, 0, 1920, 1080, SDL_WINDOW_SHOWN);
+
+	// Make sure creating the window succeeded
+	if (!mainWindow) 
+	{
+		std::cout << "Error creating window: " << SDL_GetError() << std::endl;
+		system("pause");
+		// End the program
+		return false;
+	}
+
+	mainRenderer = SDL_CreateRenderer(mainWindow, -1, SDL_RENDERER_ACCELERATED);
+
+	//White Window
+	SDL_SetRenderDrawColor(mainRenderer, 255, 255, 255, 255);
+
+	//Set the whole window to white
+	SDL_RenderClear(mainRenderer);
+
+	//Flip the buffers
+	SDL_RenderPresent(mainRenderer);
+
+	return true;
 }
